3ef0cf4324592c3889b9542c4851765489031d6e
Ellen Fawkes <fawkes@ttc.cz>
2017-07-24 09:50:58 +0200

    refs #1 - Rewrited cmds to new format in plugins

--- plugins/catsnack.js
+++ plugins/catsnack.js
@@ -2,40 +2,43 @@ exports.commands = [
   "catsnack"
 ]
 
-exports.catsnack = function(bot, metadata) {
+exports.catsnack = {
+  "description": "Give a food to our cat",
+  "exec": function(bot, metadata) {
     var rand = Math.floor((Math.random() * 10) + 1);
     var msg = "";
     switch (rand) {
       case 1:
-          msg = "Meow!";
-        break;
+      msg = "Meow!";
+      break;
       case 2:
-          msg = "Yummy!";
-        break;
+      msg = "Yummy!";
+      break;
       case 3:
-          msg = "Tak málo? Pffft. Naval ještě, " + metadata.user + "!";
-        break;
+      msg = "Tak málo? Pffft. Naval ještě, " + metadata.user + "!";
+      break;
       case 4:
-          msg = "Granule? To žrát nebudu, já chci kapsičku s tuňákem!";
-        break;
+      msg = "Granule? To žrát nebudu, já chci kapsičku s tuňákem!";
+      break;
       case 5:
-          msg = "*vyžírá misku jak zběsilá*";
-        break;
+      msg = "*vyžírá misku jak zběsilá*";
+      break;
       case 6:
-          msg = "Purrrrrrrrrrrrrrr.....";
-        break;
+      msg = "Purrrrrrrrrrrrrrr.....";
+      break;
       case 7:
-          msg = "JSEM Z VENUŠE A GRANULE JSOU POD MOJÍ ÚROVEŇ !!!";
-        break;
+      msg = "JSEM Z VENUŠE A GRANULE JSOU POD MOJÍ ÚROVEŇ !!!";
+      break;
       case 8:
-          msg = "https://memeyourfriends.com/wp-content/uploads/2016/09/hungry-cat-ate-all-my-loaf.jpg"
-        break;
+      msg = "https://memeyourfriends.com/wp-content/uploads/2016/09/hungry-cat-ate-all-my-loaf.jpg"
+      break;
       default:
-          msg = "Díky, mňauky!";
+      msg = "Díky, mňauky!";
     }
     bot.sendMessage({
-        to: metadata.channelID,
-        message: msg
+      to: metadata.channelID,
+      message: msg
     });
     console.log("I got food from %s in %s (index: %s)", metadata.user, metadata.channelID, rand);
+  }
 };
--- plugins/meowii.js
+++ plugins/meowii.js
@@ -9,29 +9,35 @@ function fetchAndSendMyGif(bot, metadata, tag) {
   var request_url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + tag;
   console.log("Request url: " + request_url + "\ttag: " + tag);
   request({
-      url: request_url,
-      json: true,
+    url: request_url,
+    json: true,
   }, function (error, response, body) {
-     var result = null;
-     if (!error && response.statusCode === 200) {
-              result = body.data.url;
-     }
-     if (result === null) {
-       result = "Je mi líto, ale něco se rozbilo. Zkus to prosím později.";
-       console.warn("An error occured while get request. Status code: " + response.statusCode);
-     }
-     bot.sendMessage({
-         to: metadata.channelID,
-         message: result
-     });
-     console.log("GIF SENT! result: " + result + "\ttag: " + tag);
-   });
+    var result = null;
+    if (!error && response.statusCode === 200) {
+      result = body.data.url;
+    }
+    if (result === null) {
+      result = "Je mi líto, ale něco se rozbilo. Zkus to prosím později.";
+      console.warn("An error occured while get request. Status code: " + response.statusCode);
+    }
+    bot.sendMessage({
+      to: metadata.channelID,
+      message: result
+    });
+    console.log("GIF SENT! result: " + result + "\ttag: " + tag);
+  });
 }
 
-exports.meow = function(bot, metadata) {
+exports.meow = {
+  "description": "Get a funny cat!",
+  "exec": function(bot, metadata) {
     fetchAndSendMyGif(bot, metadata, "cat");
+  }
 };
 
-exports.hwaii = function(bot, metadata) {
-    fetchAndSendMyGif(bot, metadata, "cute+fox");;
+exports.hwaii = {
+  "description": "Get a funny fox!",
+  "exec": function(bot, metadata) {
+    fetchAndSendMyGif(bot, metadata, "cute+fox");
+  }
 };
