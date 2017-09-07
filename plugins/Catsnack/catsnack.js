f3faa69710a25c1369604ad0bf5a55c0690efa24
Ellen Fawkes <fawkes@ttc.cz>
2017-09-07 14:55:34 +0200

    Closes #3 Configurable mumbles for catsnack

--- plugins/Catsnack/catsnack.js
+++ plugins/Catsnack/catsnack.js
@@ -7,6 +7,7 @@ const catsnackConf = CONFIG.catsnack || {};
 var overfeedThreshold;
 var overfeedDiscount;
 var overfeeded = false;
+var mumbles;
 
 exports.commands = [
   "catsnack"
@@ -26,6 +27,15 @@ exports.init = function(pluginName) {
   overfeedThreshold = catsnackConf.overfeedThreshold || 20;
   overfeedDiscount = overfeedThreshold;
 
+  mumbles = catsnackConf.mumbles || [
+    "Meow",
+    "Pffft, Bleh!",
+    "I want meat! I don't eat granules! I AM LADY, DUDE!",
+    "Purrrrrrr",
+    "Tuna?? I love tuna!!",
+    "Chew, chew"
+  ];
+
   const TIMEOUT = catsnackConf.overfeedTimeout || 60; // Overfeed timeout
   const FSB = catsnackConf.overfeedFSB || 4; // Regular heal FSB
   setInterval(healKitty, TIMEOUT * FSB * 1000);
@@ -33,6 +43,7 @@ exports.init = function(pluginName) {
 
 exports.status = function() {
   return {
+    "mumblesCount": mumbles.length,
     "overfeedDiscount": overfeedDiscount,
     "overfeedThreshold": overfeedThreshold,
     "overfeeded": overfeeded,
@@ -64,36 +75,10 @@ exports.catsnack = {
       return;
     } else overfeedDiscount--;
     logger.log("Overfeed discount: %s", overfeedDiscount);
-    var rand = Math.floor((Math.random() * 10) + 1);
+    var rand = Math.floor((Math.random() * catsnackConf.notOccureIndex || 10) + 1);
     var msg = "";
-    switch (rand) {
-      case 1:
-      msg = "Meow!";
-      break;
-      case 2:
-      msg = "Yummy!";
-      break;
-      case 3:
-      msg = "Tak málo? Pffft. Naval ještě, " + message.author + "!";
-      break;
-      case 4:
-      msg = "Granule? To žrát nebudu, já chci kapsičku s tuňákem!";
-      break;
-      case 5:
-      msg = "*vyžírá misku jak zběsilá*";
-      break;
-      case 6:
-      msg = "Purrrrrrrrrrrrrrr.....";
-      break;
-      case 7:
-      msg = "JSEM Z VENUŠE A GRANULE JSOU POD MOJÍ ÚROVEŇ !!!";
-      break;
-      case 8:
-      msg = "https://memeyourfriends.com/wp-content/uploads/2016/09/hungry-cat-ate-all-my-loaf.jpg"
-      break;
-      default:
-      msg = "Díky, mňauky!";
-    }
+    if (mumbles[rand]) msg = mumbles[rand];
+    else msg = catsnackConf.defaultMumble || "Meow! Thanks!";
     message.channel.send(msg)
     .then(logger.info(`I got food from ${message.author.username} in #${message.channel.name} (Answer index: ${rand})`))
     .catch(logger.error);
